f78d80940751ed97bbc23427bd5fd9163e2cbc67
leonardo.gabriel <leonardo.gabriel@take.net>
2022-10-31 09:50:37 -0300

    [InsecureSocketException] Added toJson method

--- lib/src/protocol/exceptions/insecure_socket.exception.dart
+++ lib/src/protocol/exceptions/insecure_socket.exception.dart
@@ -2,4 +2,8 @@ class InsecureSocketException implements Exception {
   String message;
 
   InsecureSocketException(this.message);
+
+  Map<String, dynamic> toJson() => {
+        'message': message,
+      };
 }
